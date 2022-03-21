44197e5464e187a1531afaea8bdac8f800074804
Ohminjung0804 <w2009@e-mirim.hs.kr>
2022-03-21 14:23:56 +0900

    templates: playground/ -> hello_html/ -> say_hello_html() -> html

--- playground/views.py
+++ playground/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 def say_hello(request):
     return HttpResponse("Hello, World!!!!!!!!")
 
+def say_hello_html(request):
+    return render(request, 'playground/hello.html')
+
 
